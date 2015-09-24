f0c0a0bbb146a3bf432bec017808330d9b80e52d
Gorka López de Torre <gorka@trovit.com>
2015-09-24 10:02:55 +0200

    MongoEventStore initial implementation

---
+++ spec/Infrastructure/Data/EventStore/MongoEventStoreSpec.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace spec\Gorka\Blog\Infrastructure\Data\EventStore;
+
+use Gorka\Blog\Domain\Event\Post\PostWasCreated;
+use Gorka\Blog\Domain\Event\Post\PostWasPublished;
+use Gorka\Blog\Domain\Model\AggregateHistory;
+use Gorka\Blog\Domain\Model\AggregateId;
+use Gorka\Blog\Domain\Model\Post\PostId;
+use Gorka\Blog\Infrastructure\Data\EventStore\MongoEventStore;
+use Gorka\Blog\Infrastructure\Exception\Data\DataAccessException;
+use Gorka\Blog\Infrastructure\Service\Message\Serializer\Serializer;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MongoEventStoreSpec extends ObjectBehavior
+{
+    const POST_ID = '25769c6c-d34d-4bfe-ba98-e0ee856f3e7a';
+
+    const POST_TITLE = 'Test title';
+
+    const POST_CONTENT = 'Test content';
+
+    const TEST_DB = 'testDB';
+
+    const TEST_COLLECTION = 'testCollection';
+
+    function let(Serializer $serializer, \MongoClient $mongoClient)
+    {
+        $this->beConstructedWith($serializer, $mongoClient, self::TEST_DB, self::TEST_COLLECTION);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(MongoEventStore::class);
+    }
+
+    function it_should_store_events_to_mongo_on_commit_call(
+        Serializer $serializer,
+        \MongoClient $mongoClient,
+        \MongoCollection $mongoCollection,
+        AggregateHistory $aggregateHistory
+    ) {
+        $postId = PostId::create(self::POST_ID);
+        $events = $this->buildTestEvents($postId);
+
+        $aggregateHistory->events()->willReturn(array_column($events, 'event'));
+
+        $serializedEvents = [];
+        foreach ($events as $event) {
+            $serializedEvent = $event['array'];
+            $serializedEvents[] = $serializedEvent;
+            $serializer->serialize($event['event'])->willReturn(json_encode($serializedEvent));
+        }
+
+        $mongoClient->selectCollection(self::TEST_DB, self::TEST_COLLECTION)->willReturn($mongoCollection);
+        $mongoCollection->batchInsert($serializedEvents)->shouldBeCalled();
+
+        $this->commit($aggregateHistory);
+    }
+
+    function it_should_throw_exception_on_commit_failure(\MongoClient $mongoClient, AggregateHistory $aggregateHistory)
+    {
+        $mongoClient->selectCollection()->willThrow(new \Exception());
+        $this->shouldThrow(DataAccessException::class)->during('commit', [$aggregateHistory]);
+    }
+
+    function it_should_throw_exception_on_events_call_failure(\MongoClient $mongoClient, AggregateId $aggregateId)
+    {
+        $mongoClient->selectCollection()->willThrow(new \Exception());
+        $this->shouldThrow(DataAccessException::class)->during('events', [$aggregateId]);
+    }
+
+    function it_should_retrieve_events_from_mongo_on_events_call(
+        AggregateId $aggregateId,
+        Serializer $serializer,
+        \MongoClient $mongoClient,
+        \MongoCollection $mongoCollection,
+        \MongoCursor $mongoCursor
+    ) {
+        $postId = PostId::create(self::POST_ID);
+        $aggregateId->id()->willReturn($postId);
+        $events = $this->buildTestEvents($postId);
+
+        $mongoClient->selectCollection(self::TEST_DB, self::TEST_COLLECTION)->willReturn($mongoCollection);
+        $mongoCollection->find(['message.payload.id' => self::POST_ID])->willReturn($mongoCursor);
+        $mongoCursor->sort(['creation_time' => 1])->willReturn(array_column($events, 'array'));
+
+        foreach ($events as $event) {
+            $serializer->deserialize(json_encode($event['array'], true))->willReturn($event['event']);
+        }
+
+        $this->events($aggregateId)->shouldReturn(array_column($events, 'event'));
+    }
+
+    /**
+     * @param $postId
+     * @return array
+     */
+    private function buildTestEvents($postId)
+    {
+        $eventCreate = new PostWasCreated(
+            $postId,
+            self::POST_TITLE,
+            self::POST_CONTENT
+        );
+        $eventCreateArray = [
+            'message' => [
+                'type' => 'blog:post_was_created',
+                'payload' => [
+                    'id' => self::POST_ID,
+                    'title' => self::POST_TITLE,
+                    'content' => self::POST_CONTENT
+                ]
+            ],
+            'creation_time' => '2015-02-03T12:34:56+0100'
+        ];
+
+        $eventPublish = new PostWasPublished($postId);
+        $eventPublishArray = [
+            'message' => [
+                'type' => 'blog:post_was_published',
+                'payload' => [
+                    'id' => self::POST_ID
+                ]
+            ],
+            'creation_time' => '2015-02-03T12:34:58+0100'
+        ];
+
+        return [
+            [
+                'event' => $eventCreate,
+                'array' => $eventCreateArray
+            ],
+            [
+                'event' => $eventPublish,
+                'array' => $eventPublishArray
+            ]
+        ];
+    }
+}
---
+++ src/Infrastructure/Data/EventStore/MongoEventStore.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Gorka\Blog\Infrastructure\Data\EventStore;
+
+use Assert\Assertion;
+use Gorka\Blog\Domain\Event\DomainEvent;
+use Gorka\Blog\Domain\Model\AggregateHistory;
+use Gorka\Blog\Domain\Model\AggregateId;
+use Gorka\Blog\Domain\Model\EventStore;
+use Gorka\Blog\Infrastructure\Exception\Data\DataAccessException;
+use Gorka\Blog\Infrastructure\Exception\Data\DataNotFoundException;
+use Gorka\Blog\Infrastructure\Service\Message\Serializer\Serializer;
+
+class MongoEventStore implements EventStore
+{
+
+    /**
+     * @var Serializer
+     */
+    private $serializer;
+    /**
+     * @var \MongoClient
+     */
+    private $mongoClient;
+
+    /**
+     * @var string
+     */
+    private $databaseName;
+
+    /**
+     * @var string
+     */
+    private $collectionName;
+
+    public function __construct(Serializer $serializer, \MongoClient $mongoClient, $databaseName, $collectionName)
+    {
+        $this->serializer = $serializer;
+        $this->mongoClient = $mongoClient;
+        $this->setDatabaseName($databaseName);
+        $this->setCollectionName($collectionName);
+    }
+
+    /**
+     * @param AggregateHistory $history
+     * @throws DataAccessException
+     */
+    public function commit(AggregateHistory $history)
+    {
+        try {
+            $eventBatch = $this->prepareEventBatch($history->events());
+            $this->mongoClient
+                ->selectCollection($this->databaseName, $this->collectionName)
+                ->batchInsert($eventBatch);
+            ;
+        } catch (\Exception $e) {
+            throw new DataAccessException($e->getMessage());
+        }
+    }
+
+    /**
+     * @param AggregateId $id
+     * @return DomainEvent[]
+     * @throws DataNotFoundException
+     * @throws DataAccessException
+     */
+    public function events(AggregateId $id)
+    {
+        try {
+            $serializedEvents = $this->mongoClient
+                ->selectCollection($this->databaseName, $this->collectionName)
+                ->find(['message.payload.id' => $id->id()])
+                ->sort(['creation_time' => 1])
+            ;
+
+            $events = [];
+            foreach ($serializedEvents as $serializedEvent) {
+                $events[] = $this->serializer->deserialize(json_encode($serializedEvent));
+            }
+            return $events;
+        } catch (\Exception $e) {
+            throw new DataAccessException($e->getMessage());
+        }
+    }
+
+    /**
+     * @param $databaseName
+     */
+    private function setDatabaseName($databaseName)
+    {
+        Assertion::string($databaseName);
+        Assertion::notBlank(trim($databaseName));
+        $this->databaseName = $databaseName;
+    }
+
+    /**
+     * @param $collectionName
+     */
+    private function setCollectionName($collectionName)
+    {
+        Assertion::string($collectionName);
+        Assertion::notBlank(trim($collectionName));
+        $this->collectionName = $collectionName;
+    }
+
+    private function prepareEventBatch($events)
+    {
+        $serializedEvents = [];
+        foreach ($events as $event) {
+            $serializedEvents[] = json_decode($this->serializer->serialize($event), true);
+        }
+        return $serializedEvents;
+    }
+}
